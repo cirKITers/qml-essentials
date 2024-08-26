4d51a7d0fefe9c3705bb210d74d7e395edbc287e
Melvin Strobl <lc3267@kit.edu>
2024-08-26 09:42:48 +0200

    added check if spectrum is real
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- qml_essentials/coefficients.py
+++ qml_essentials/coefficients.py
@@ -6,9 +6,15 @@ import numpy as np
 
 class Coefficients:
 
+    @staticmethod
     def sample_coefficients(model: Model) -> np.ndarray:
         """
-        Sample the Fourier coefficients of a given model.
+        Sample the Fourier coefficients of a given model
+        using Pennylane fourier.coefficients function.
+
+        Note that the coefficients are complex numbers, but the imaginary part
+        of the coefficients should be very close to zero, since the expectation
+        values of the Pauli operators are real numbers.
 
         Args:
             model (Model): The model to sample.
@@ -17,4 +23,11 @@ class Coefficients:
             np.ndarray: The sampled Fourier coefficients.
         """
         partial_circuit = partial(model, model.params)
-        return coefficients(partial_circuit, 1, model.degree)
+        coeffs = coefficients(partial_circuit, 1, model.degree)
+
+        if not np.isclose(np.sum(coeffs).imag, 0.0, rtol=1.0e-5):
+            raise ValueError(
+                f"Spectrum is not real. Imaginary part of coefficients is: {np.sum(coeffs).imag}"
+            )
+
+        return coeffs
