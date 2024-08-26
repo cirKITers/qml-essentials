0ce2cdf44fe7f456a7378f7d0cc91cdfb3d65e3f
Melvin Strobl <lc3267@kit.edu>
2024-08-26 10:23:13 +0200

    added coefficient test
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

---
+++ tests/test_coefficients.py
@@ -0,0 +1,53 @@
+from qml_essentials.model import Model
+from qml_essentials.coefficients import Coefficients
+
+import pytest
+import numpy as np
+import logging
+import math
+
+logger = logging.getLogger(__name__)
+
+
+def test_coefficients() -> None:
+    test_cases = [
+        {
+            "circuit_type": "Circuit_1",
+            "n_qubits": 1,
+            "n_layers": 3,
+            "n_bins": 10,
+            "n_samples": 200,
+            "n_input_samples": 2,
+            "result": 1.858,
+        },
+        {
+            "circuit_type": "Circuit_9",
+            "n_qubits": 4,
+            "n_layers": 1,
+            "n_bins": 10,
+            "n_samples": 200,
+            "n_input_samples": 2,
+            "result": 2.629,
+        },
+    ]
+
+    for test_case in test_cases:
+        model = Model(
+            n_qubits=test_case["n_qubits"],
+            n_layers=test_case["n_layers"],
+            circuit_type=test_case["circuit_type"],
+            data_reupload=True,
+            initialization="random",
+            output_qubit=0,
+        )
+
+        coeffs = Coefficients.sample_coefficients(model)
+
+        assert len(coeffs) == model.degree * 2 + 1, "Wrong number of coefficients"
+        assert np.isclose(
+            np.sum(coeffs).imag, 0.0, rtol=1.0e-5
+        ), "Imaginary part is not zero"
+
+
+if __name__ == "__main__":
+    test_coefficients()
