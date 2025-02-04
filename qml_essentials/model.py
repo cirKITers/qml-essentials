211f77d2cc3738e4ab6e0cecbe0bc1564336c8de
Melvin Strobl <lc3267@kit.edu>
2025-02-04 10:28:19 +0100

    removed check for torch layer
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- qml_essentials/model.py
+++ qml_essentials/model.py
@@ -438,7 +438,7 @@ class Model:
             raise ValueError(f"Invalid execution_type: {self.execution_type}.")
 
     def _draw(self, inputs=None, figure=False) -> None:
-        if isinstance(self.circuit, qml.qnn.torch.TorchLayer):
+        if not isinstance(self.circuit, qml.qnn.torch.TorchLayer):
             # TODO: throws strange argument error if not catched
             return ""
 
@@ -636,7 +636,7 @@ class Model:
                     inputs=inputs,
                 )
             else:
-                if isinstance(self.circuit, qml.qnn.torch.TorchLayer):
+                if not isinstance(self.circuit, qml.qnn.torch.TorchLayer):
                     result = self.circuit(
                         inputs=inputs,
                     )
@@ -655,7 +655,7 @@ class Model:
             # discard gradient information
             if force_mean:
                 # exception for torch layer because it swaps batch and output dimension
-                if isinstance(self.circuit, qml.qnn.torch.TorchLayer):
+                if not isinstance(self.circuit, qml.qnn.torch.TorchLayer):
                     result = result.mean(axis=-1)
                 else:
                     result = result.mean(axis=0)
