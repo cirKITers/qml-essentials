9aaea455dccfa2a51944d40ef7867c2c49d00f01
paxuy <luap.schillinger@gmail.com>
2025-02-12 22:17:58 +0100

    implements fr + nice live printing

--- tests/test_entanglement.py
+++ tests/test_entanglement.py
@@ -12,104 +12,67 @@ logger = logging.getLogger(__name__)
 @pytest.mark.unittest
 def test_entanglement() -> None:
     # Results taken from: https://doi.org/10.1002/qute.201900070
-    # circuits = [1, 7, 3, 16, 8, 5, 18, 17, 4, 10, 19, 13, 12, 14, 11, 6, 2, 15, 9]
-    # ent_res = [0.0000, 0.3246, 0.3424, 0.3464, 0.3932, 0.4099, 0.4383, 0.4541,
-    # 0.4715, 0.5369, 0.5937, 0.6070, 0.6487, 0.6613, 0.7330, 0.7803, 0.8083,
-    # 0.8186, 1.0000]
+    circuits = [1, 7, 3, 16, 8, 5, 18, 17, 4, 10, 19, 13, 12, 14, 11, 6, 2, 15, 9]
+    ent_results = [
+        0.0000,
+        0.3246,
+        0.3424,
+        0.3464,
+        0.3932,
+        0.4099,
+        0.4383,
+        0.4541,
+        0.4715,
+        0.5369,
+        0.5937,
+        0.6070,
+        0.6487,
+        0.6613,
+        0.7330,
+        0.7803,
+        0.8083,
+        0.8186,
+        1.0000,
+    ]
+    no_ent_result = 0.0
+    strongly_ent_result = 0.8379
 
     # Circuits [5,7,8,11,12,13,14] are not included in the test cases,
     # because not implemented in ansaetze.py
 
     # Circuit 10 excluded because implementation with current setup not possible
+    skip_indices = [5, 7, 8, 11, 12, 13, 14, 10]
+    # skip_indices = [7, 3, 16, 8, 5, 18, 17, 4, 10, 19, 13, 12, 14, 11, 6, 2, 15, 9]
     test_cases = [
         {
             "circuit_type": "No_Entangling",
             "n_qubits": 4,
             "n_layers": 1,
-            "result": 0.0,
+            "result": no_ent_result,
         },
         {
             "circuit_type": "Strongly_Entangling",
             "n_qubits": 4,
             "n_layers": 1,
-            "result": 0.8379,
-        },
-        {
-            "circuit_type": "Circuit_1",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.0000,
-        },
-        {
-            "circuit_type": "Circuit_3",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.3424,
-        },
-        {
-            "circuit_type": "Circuit_16",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.3464,
-        },
-        {
-            "circuit_type": "Circuit_18",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.4383,
-        },
-        {
-            "circuit_type": "Circuit_17",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.4541,
-        },
-        {
-            "circuit_type": "Circuit_4",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.4715,
-        },
-        # {
-        #     "circuit_type": "Circuit_10",
-        #     "n_qubits": 4,
-        #     "n_layers": 1,
-        #     "result": 0.5369,
-        # },
-        {
-            "circuit_type": "Circuit_19",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.5937,
-        },
-        {
-            "circuit_type": "Circuit_6",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.7803,
-        },
-        {
-            "circuit_type": "Circuit_2",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.8083,
-        },
-        {
-            "circuit_type": "Circuit_15",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.8186,
-        },
-        {
-            "circuit_type": "Circuit_9",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 1.0000,
+            "result": strongly_ent_result,
         },
     ]
+    for i, ent_res in zip(circuits, ent_results):
+        if i in skip_indices:
+            continue
+        test_cases.append(
+            {
+                "circuit_type": f"Circuit_{i}",
+                "n_qubits": 4,
+                "n_layers": 1,
+                "result": ent_res,
+            }
+        )
 
     tolerance = 0.55  # FIXME: reduce when reason for discrepancy is found
+    ent_caps: list[tuple[int, float]] = []
     for test_case in test_cases:
+        print(f"--- Running Entanglement test for {test_case['circuit_type']} ---")
         model = Model(
             n_qubits=test_case["n_qubits"],
             n_layers=test_case["n_layers"],
@@ -122,17 +85,48 @@ def test_entanglement() -> None:
             model, n_samples=5000, seed=1000, cache=False
         )
 
+        circuit_number = 0
+        if test_case["circuit_type"] == "No_Entangling":
+            circuit_number = -1
+        elif test_case["circuit_type"] == "Strongly_Entangling":
+            circuit_number = -2
+        else:
+            circuit_number = int(test_case["circuit_type"].split("_")[1])
+        ent_caps.append((circuit_number, ent_cap))
+
         difference = abs(ent_cap - test_case["result"])
         if math.isclose(difference, 0.0, abs_tol=1e-3):
             error = 0
         else:
             error = abs(ent_cap - test_case["result"]) / (test_case["result"])
 
+        print(
+            f"Entangling-capability: {ent_cap}, Expected Result: {test_case['result']}, Error: {error}"
+        )
         assert (
             error < tolerance
         ), f"Entangling-capability of circuit {test_case['circuit_type']} is not\
             {test_case['result']} but {ent_cap} instead.\
             Deviation {(error*100):.1f}%>{tolerance*100}%"
+
+    expected_ent_results = sorted(
+        [(-1, no_ent_result), (-2, strongly_ent_result)]
+        + [
+            (circuit, ent_results[circuits.index(circuit)])
+            for circuit in circuits
+            if circuit not in skip_indices
+        ],
+        key=lambda x: x[1],
+    )
+
+    actual_ent_results = sorted(ent_caps, key=lambda x: x[1])
+
+    print("Expected \t| Actual")
+    for expected, actual in zip(expected_ent_results, actual_ent_results):
+        print(f"{expected[0]}, {expected[1]} \t| {actual[0]}, {actual[1]}")
+    assert [circuit for circuit, _ in actual_ent_results] == [
+        circuit for circuit, _ in expected_ent_results
+    ]
 
 
 @pytest.mark.smoketest
--- tests/test_expressiblity.py
+++ tests/test_expressiblity.py
@@ -45,167 +45,81 @@ def test_divergence() -> None:
 @pytest.mark.expensive
 def test_expressibility() -> None:
     # Results taken from: https://doi.org/10.1002/qute.201900070
-    # circuits = [9, 1, 2, 16, 3, 18, 10, 12, 15, 17, 4, 11, 7, 8, 19, 5, 13, 14, 6]
-    # results-n_layers-1 = [0.6773, 0.2999, 0.2860, 0.2602, 0.2396, 0.2340, 0.2286,
-    # 0.1984, 0.1892, 0.1359, 0.1343, 0.1312, 0.0977, 0.0858, 0.0809, 0.0602,
-    # 0.0516, 0.0144, 0.0043]
-    # results-n_layers-3 = [0.0322, 0.2079, 0.0084, 0.0375, 0.0403, 0.0221, 0.1297,
-    # 0.0089, 0.1152, 0.0180, 0.0107, 0.0038, 0.0162, 0.0122, 0.0040, 0.0030,
-    # 0.0049, 0.0035, 0.0039]
+    circuits = [9, 1, 2, 16, 3, 18, 10, 12, 15, 17, 4, 11, 7, 8, 19, 5, 13, 14, 6]
+    results_n_layers_1 = [
+        0.6773,
+        0.2999,
+        0.2860,
+        0.2602,
+        0.2396,
+        0.2340,
+        0.2286,
+        0.1984,
+        0.1892,
+        0.1359,
+        0.1343,
+        0.1312,
+        0.0977,
+        0.0858,
+        0.0809,
+        0.0602,
+        0.0516,
+        0.0144,
+        0.0043,
+    ]
+
+    results_n_layers_3 = [
+        0.0322,
+        0.2079,
+        0.0084,
+        0.0375,
+        0.0403,
+        0.0221,
+        0.1297,
+        0.0089,
+        0.1152,
+        0.0180,
+        0.0107,
+        0.0038,
+        0.0162,
+        0.0122,
+        0.0040,
+        0.0030,
+        0.0049,
+        0.0035,
+        0.0039,
+    ]
 
     # Circuits [5,7,8,11,12,13,14] are not included in the test cases,
     # because not implemented in ansaetze.py
 
     # Circuit 10 excluded because implementation with current setup not possible
-    test_cases = [
-        {
-            "circuit_type": "Circuit_9",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.6773,
-        },
-        # {
-        #     "circuit_type": "Circuit_9",
+    skip_indices = [5, 7, 8, 11, 12, 13, 14, 10]
+    skip_indices = [16, 3, 18, 10, 12, 15, 17, 4, 11, 7, 8, 19, 5, 13, 14, 6]
+
+    test_cases = []
+    for i, result1, result3 in zip(circuits, results_n_layers_1, results_n_layers_3):
+        if i in skip_indices:
+            continue
+        test_cases.append(
+            {
+                "circuit_type": f"Circuit_{i}",
+                "n_qubits": 4,
+                "n_layers": 1,
+                "result": result1,
+            }
+        )
+        # test_cases.append({
+        #     "circuit_type": f"Circuit_{i}",
         #     "n_qubits": 4,
         #     "n_layers": 3,
-        #     "result": 0.0322,
-        # },
-        {
-            "circuit_type": "Circuit_1",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.2999,
-        },
-        # {
-        #     "circuit_type": "Circuit_1",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.2079,
-        # },
-        {
-            "circuit_type": "Circuit_2",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.2860,
-        },
-        # {
-        #     "circuit_type": "Circuit_2",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0084,
-        # },
-        {
-            "circuit_type": "Circuit_16",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.2602,
-        },
-        # {
-        #     "circuit_type": "Circuit_16",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0375,
-        # },
-        {
-            "circuit_type": "Circuit_3",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.2396,
-        },
-        # {
-        #     "circuit_type": "Circuit_3",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0403,
-        # },
-        {
-            "circuit_type": "Circuit_18",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.2340,
-        },
-        # {
-        #     "circuit_type": "Circuit_18",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0221,
-        # },
-        # {
-        #     "circuit_type": "Circuit_10",
-        #     "n_qubits": 4,
-        #     "n_layers": 1,
-        #     "result": 0.2286,
-        # },
-        # {
-        #     "circuit_type": "Circuit_10",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.1297,
-        # },
-        {
-            "circuit_type": "Circuit_15",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.1892,
-        },
-        # {
-        #     "circuit_type": "Circuit_15",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.1152,
-        # },
-        {
-            "circuit_type": "Circuit_17",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.1359,
-        },
-        # {
-        #     "circuit_type": "Circuit_17",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0180,
-        # },
-        {
-            "circuit_type": "Circuit_4",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.1343,
-        },
-        # {
-        #     "circuit_type": "Circuit_4",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0107,
-        # },
-        {
-            "circuit_type": "Circuit_19",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.0809,
-        },
-        # {
-        #     "circuit_type": "Circuit_19",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0040,
-        # },
-        {
-            "circuit_type": "Circuit_6",
-            "n_qubits": 4,
-            "n_layers": 1,
-            "result": 0.0043,
-        },
-        # {
-        #     "circuit_type": "Circuit_6",
-        #     "n_qubits": 4,
-        #     "n_layers": 3,
-        #     "result": 0.0039,
-        # },
-    ]
+        #     "result": result3,
+        # })
 
     tolerance = 0.35  # FIXME: reduce when reason for discrepancy is found
+    kl_distances: list[tuple[int, float]] = []
     for test_case in test_cases:
+        print(f"--- Running Expressibility test for {test_case['circuit_type']} ---")
         model = Model(
             n_qubits=test_case["n_qubits"],
             n_layers=test_case["n_layers"],
@@ -232,17 +146,41 @@ def test_expressibility() -> None:
         # Calculate the mean (over all inputs, if required)
         kl_dist = Expressibility.kullback_leibler_divergence(z, y_haar).mean()
 
+        circuit_number = int(test_case["circuit_type"].split("_")[1])
+        kl_distances.append((circuit_number, kl_dist.item()))
+
         difference = abs(kl_dist - test_case["result"])
         if math.isclose(difference, 0.0, abs_tol=1e-10):
             error = 0
         else:
             error = abs(kl_dist - test_case["result"]) / (test_case["result"])
 
+        print(
+            f"KL Divergence: {kl_dist}, Expected Result: {test_case['result']}, Error: {error}"
+        )
         assert (
             error < tolerance
         ), f"Expressibility of circuit {test_case['circuit_type']} is not\
             {test_case['result']} but {kl_dist} instead.\
             Deviation {(error*100):.1f}>{tolerance*100}%"
+
+    circuits_results_1 = sorted(
+        [
+            (circuit, results_n_layers_1[circuits.index(circuit)])
+            for circuit in circuits
+            if circuit not in skip_indices
+        ],
+        key=lambda x: x[1],
+    )
+
+    kl_distances_sorted = sorted(kl_distances, key=lambda x: x[1])
+
+    print("Expected \t| Actual")
+    for expected, actual in zip(circuits_results_1, kl_distances_sorted):
+        print(f"{expected[0]}, {expected[1]} \t| {actual[0]}, {actual[1]}")
+    assert [circuit for circuit, _ in circuits_results_1] == [
+        circuit for circuit, _ in kl_distances_sorted
+    ]
 
 
 @pytest.mark.unittest
