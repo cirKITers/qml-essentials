11a1c14bcd62246ebc797acc687fd83f22a4a0d9
Melvin Strobl <lc3267@kit.edu>
2025-03-04 14:33:45 +0100

    added proper test
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- tests/test_entanglement.py
+++ tests/test_entanglement.py
@@ -8,10 +8,9 @@ import pytest
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.expensive
-@pytest.mark.unittest
-def test_entanglement() -> None:
+def get_test_cases():
     # Results taken from: https://doi.org/10.1002/qute.201900070
+
     circuits = [
         "No_Entangling",
         "Strongly_Entangling",
@@ -64,6 +63,15 @@ def test_entanglement() -> None:
     # Circuit 10 excluded because implementation with current setup not possible
     skip_indices = [5, 7, 8, 11, 12, 13, 14, 10]
     skip_indices += [2, 3]  # exclude these for now as order is failing
+
+    return circuits, results_n_layers_1, skip_indices
+
+
+@pytest.mark.expensive
+@pytest.mark.unittest
+def test_mw_measure() -> None:
+    circuits, results_n_layers_1, skip_indices = get_test_cases()
+
     test_cases = []
     for circuit_id, res_1l in zip(circuits, results_n_layers_1):
         if circuit_id in skip_indices:
@@ -151,30 +159,94 @@ def test_no_sampling() -> None:
         n_qubits=2,
         n_layers=1,
         circuit_type="Hardware_Efficient",
-        data_reupload=True,
+        data_reupload=False,
         initialization="random",
     )
 
     _ = Entanglement.meyer_wallach(model, n_samples=-1, seed=1000, cache=False)
 
 
-def test_bell_measurements() -> None:
-    model = Model(
-        n_qubits=2,
-        n_layers=1,
-        circuit_type="Circuit_19",
-        data_reupload=True,
-        initialization="random",
+@pytest.mark.expensive
+@pytest.mark.unittest
+def test_bell_measure() -> None:
+    circuits, results_n_layers_1, skip_indices = get_test_cases()
+
+    test_cases = []
+    for circuit_id, res_1l in zip(circuits, results_n_layers_1):
+        if circuit_id in skip_indices:
+            continue
+        if isinstance(circuit_id, int):
+            test_cases.append(
+                {
+                    "circuit_type": f"Circuit_{circuit_id}",
+                    "n_qubits": 4,
+                    "n_layers": 1,
+                    "result": res_1l,
+                }
+            )
+        elif isinstance(circuit_id, str):
+            test_cases.append(
+                {
+                    "circuit_type": circuit_id,
+                    "n_qubits": 4,
+                    "n_layers": 1,
+                    "result": res_1l,
+                }
+            )
+
+    tolerance = 0.55  # FIXME: reduce when reason for discrepancy is found
+    ent_caps: list[tuple[str, float]] = []
+    for test_case in test_cases:
+        print(f"--- Running Entanglement test for {test_case['circuit_type']} ---")
+        model = Model(
+            n_qubits=test_case["n_qubits"],
+            n_layers=test_case["n_layers"],
+            circuit_type=test_case["circuit_type"],
+            data_reupload=False,
+            initialization="random",
+        )
+
+        ent_cap = Entanglement.bell_measurements(
+            model, n_samples=5000, seed=1000, cache=False
+        )
+
+        # Save results for later comparison
+        circuit_number = test_case["circuit_type"]
+        if circuit_number.split("_")[1].isdigit():
+            circuit_number = int(circuit_number.split("_")[1])
+        ent_caps.append((circuit_number, ent_cap))
+
+        difference = abs(ent_cap - test_case["result"])
+        if math.isclose(difference, 0.0, abs_tol=1e-3):
+            error = 0
+        else:
+            error = abs(ent_cap - test_case["result"]) / (test_case["result"])
+
+        print(
+            f"Entangling-capability: {ent_cap},\t"
+            + f"Expected Result: {test_case['result']},\t"
+            + f"Error: {error}"
+        )
+        assert (
+            error < tolerance
+        ), f"Entangling-capability of circuit {test_case['circuit_type']} is not\
+            {test_case['result']} but {ent_cap} instead.\
+            Deviation {(error*100):.1f}%>{tolerance*100}%"
+
+    references = sorted(
+        [
+            (circuit, ent_result)
+            for circuit, ent_result in zip(circuits, results_n_layers_1)
+            if circuit not in skip_indices
+        ],
+        key=lambda x: x[1],
     )
 
-    ent_cap_bell = Entanglement.bell_measurements(
-        model, n_samples=5000, seed=1000, cache=False
-    )
+    actuals = sorted(ent_caps, key=lambda x: x[1])
 
-    ent_cap = Entanglement.meyer_wallach(model, n_samples=5000, seed=1000, cache=False)
-
-    print(f"Bell: {ent_cap_bell} | Reference: {ent_cap}")
-
-
-if __name__ == "__main__":
-    test_bell_measurements()
+    print("Expected \t| Actual")
+    for reference, actual in zip(references, actuals):
+        print(f"{reference[0]}, {reference[1]} \t| {actual[0]}, {actual[1]}")
+    assert [circuit for circuit, _ in actuals] == [
+        circuit for circuit, _ in references
+    ], f"Order of circuits does not match: {actuals} != {references}"
