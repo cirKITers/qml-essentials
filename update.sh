28567795cd050d7c03643e0480b6dea4a4907ba5
Melvin Strobl <lc3267@kit.edu>
2025-04-15 09:36:41 +0200

    updates
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
+# Convenience script to update all major releases
+
 poetry add pennylane@latest
+poetry add dill@latest
+
 poetry add black@latest --group dev
 poetry add pytest@latest --group dev
 poetry add flake8@latest --group dev
@@ -6,11 +10,13 @@ poetry add pre-commit@latest --group dev
 poetry add coverage@latest --group dev
 poetry add licensecheck@latest --group dev
 poetry add matplotlib@latest --group dev
+poetry add pytest-xdist@latest --group dev
 
 poetry add mkdocs@latest --group docs
 poetry add mkdocs-material@latest --group docs
+poetry add mkdocs-glightbox@latest --group docs
 poetry add mkdocstrings-python@latest --group docs
 poetry add markdown-include@latest --group docs
-poetry add ipykernel --group docs
+poetry add ipykernel@latest --group docs
 
 poetry update
