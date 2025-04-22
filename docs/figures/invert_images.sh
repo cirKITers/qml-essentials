74d1552afa53cc34551fc51baace57caef042b52
Melvin Strobl <lc3267@kit.edu>
2025-04-22 17:52:36 +0200

    updated background
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- docs/figures/invert_images.sh
+++ docs/figures/invert_images.sh
@@ -1,4 +1,4 @@
 # iterate all *.png files in current directory
 for file in *_light.png; do
-  magick convert "$file" -channel RGB -negate "${file/_light.png/_dark.png}"
+  magick "$file" -transparent white -channel RGB -negate "${file/_light.png/_dark.png}"
 done
