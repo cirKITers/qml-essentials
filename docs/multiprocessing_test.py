53ad67131835e866a8ee8873343a8a358c8834ba
Melvin Strobl <lc3267@kit.edu>
2025-07-08 22:43:54 +0300

    plotting
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- docs/multiprocessing_test.py
+++ docs/multiprocessing_test.py
@@ -2,14 +2,16 @@ import time
 import pennylane.numpy as np
 import json
 from qml_essentials.model import Model
+import matplotlib.pyplot as plt
 
+seed = 1000
 min_n_samples = 500
-max_n_samples = 5000
-n_samples_step = 500
+max_n_samples = 8500
+n_samples_step = 1000
 min_n_qubits = 2
-max_n_qubits = 6
+max_n_qubits = 8
 n_layers = 1
-n_runs = 5
+n_runs = 10
 
 time_measure = time.time
 
@@ -22,44 +24,95 @@ print(f"min_n_qubits: {min_n_qubits}")
 print(f"max_n_qubits: {max_n_qubits}")
 print(f"n_runs: {n_runs}")
 
-results = {}
+try:
+    with open("results.json", "r") as f:
+        results = json.load(f)
+except FileNotFoundError:
+    results = {}
+    pass
 
-for run in range(n_runs):
-    results[run] = {}
+if len(results) == 0:
     for n_qubits in range(min_n_qubits, max_n_qubits + 1):
-        results[run][n_qubits] = {}
+        results[n_qubits] = {}
         for n_samples in range(min_n_samples, max_n_samples + 1, n_samples_step):
-            model = Model(
-                n_qubits=n_qubits,
-                n_layers=n_layers,
-                circuit_type="Circuit_19",
-                random_seed=1000,
-            )
-            model.initialize_params(rng=np.random.default_rng(1000), repeat=n_samples)
+            results[n_qubits][n_samples] = {}
+            for run in range(n_runs):
+                model = Model(
+                    n_qubits=n_qubits,
+                    n_layers=n_layers,
+                    circuit_type="Circuit_19",
+                    random_seed=seed,
+                )
+                model.initialize_params(
+                    rng=np.random.default_rng(seed), repeat=n_samples
+                )
 
-            start = time_measure()
-            model(execution_type="density")
-            t_single = time_measure() - start
+                start = time_measure()
+                model(execution_type="density")
+                t_single = time_measure() - start
 
-            model = Model(
-                n_qubits=n_qubits,
-                n_layers=n_layers,
-                circuit_type="Circuit_19",
-                mp_threshold=1000,
-                random_seed=1000,
-            )
+                model = Model(
+                    n_qubits=n_qubits,
+                    n_layers=n_layers,
+                    circuit_type="Circuit_19",
+                    mp_threshold=1000,
+                    random_seed=seed,
+                )
 
-            model.initialize_params(rng=np.random.default_rng(1000), repeat=n_samples)
+                model.initialize_params(
+                    rng=np.random.default_rng(seed), repeat=n_samples
+                )
 
-            start = time_measure()
-            model(execution_type="density")
-            t_parallel = time_measure() - start
+                start = time_measure()
+                model(execution_type="density")
+                t_parallel = time_measure() - start
 
-            print(
-                f"{run} | {n_qubits} qubits | {n_samples} samples: {t_parallel / t_single}"
-            )
+                print(
+                    f"{run} | {n_qubits} qubits | {n_samples} samples: {t_single / t_parallel}"
+                )
 
-            results[run][n_qubits][n_samples] = t_parallel / t_single
+                results[n_qubits][n_samples][run] = t_single / t_parallel
 
-with open("results.json", "w") as f:
-    json.dump(results, f)
+    with open("results.json", "w") as f:
+        json.dump(results, f)
+
+
+for n_qubits in results.keys():
+    y_mean = []
+    y_max = []
+    y_min = []
+    for n_samples in results[n_qubits].keys():
+        samples = list(results[n_qubits][n_samples].values())
+        y_mean.append(np.mean(samples))
+
+    std = np.std(y_mean)
+    for y_mean_i in y_mean:
+        y_max.append(y_mean_i + std)
+        y_min.append(y_mean_i - std)
+
+    plt.plot(
+        list(results[n_qubits].keys()),
+        y_mean,
+        label=f"{n_qubits} qubits",
+        linestyle="dashed",
+    )
+
+    plt.fill_between(
+        list(results[n_qubits].keys()),
+        y_min,
+        y_max,
+        alpha=0.2,
+    )
+
+    # plt.errorbar(
+    #     list(results[n_qubits].keys()),
+    #     y_mean,
+    #     yerr=[[y_min_i for y_min_i in y_min], [y_max_i for y_max_i in y_max]],
+    #     label=f"{n_qubits} qubits",
+    # )
+
+
+plt.xlabel("Number of samples")
+plt.ylabel("Speedup")
+plt.legend()
+plt.savefig("figures/speedup_light.svg", dpi=100, transparent=True)
