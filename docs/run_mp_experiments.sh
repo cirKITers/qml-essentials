eb7fc5507be55530754538c43f1aa6c466a085f1
majafranz <majafranz99@gmail.com>
2026-02-03 10:06:58 +0100

    update density mp results

--- docs/run_mp_experiments.sh
+++ docs/run_mp_experiments.sh
@@ -2,4 +2,4 @@
 uv run python mp_experiment.py --seed 1000 --execution_type expval --min_n_samples 500 --max_n_samples 100000 --n_samples_step 2500 --n_qubits 4 --n_layers 1 --n_runs 1
 
 # density
-uv run python mp_experiment.py --seed 1000 --execution_type density --min_n_samples 500 --max_n_samples 30000 --n_samples_step 5000 --n_qubits 4 --n_layers 1 --n_runs 1
+uv run python mp_experiment.py --seed 1000 --execution_type density --min_n_samples 500 --max_n_samples 10000 --n_samples_step 1000 --n_qubits 4 --n_layers 1 --n_runs 1
