a7cf9835196cf5456f1afbc5ff0fc56c977ab892
lc3267 <lc3267@kit.edu>
2026-02-24 16:18:00 +0100

    benchmark
    Signed-off-by: lc3267 <lc3267@kit.edu>

---
+++ docs/slurm_benchmark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 
+# name of the job for better recognizing it in the queue overview
+#SBATCH --job-name=qml-essentials
+# 
+# define how many nodes we need
+#SBATCH --nodes=1
+#
+# we only need on 1 cpu at a time
+#SBATCH --ntasks=10
+#
+# expected duration of the job
+#              hh:mm:ss
+#SBATCH --time=03:00:00
+# 
+# partition the job will run on
+#SBATCH --partition cpu
+# 
+# expected memory requirements
+#SBATCH --mem=370GB
+#
+# infos
+#
+# output path
+#SBATCH --output="logs/slurm/slurm-%j-%x.out"
+
+module load compiler/llvm
+module load devel/python/3.12.3
+
+cd ~/qml-essentials
+uv run python docs/benchmarks.py
